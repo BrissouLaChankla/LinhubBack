925bce834c698cb0678a86268a2c7044a008324b
Clément <pozo.clement@gmail.com>
2023-02-28 16:59:27 +0100

    amelioration code generalInfo + création bdd collection et schema education avec routes educations get/post/delete + ajouts commentaires avec eplications des codes

--- routes/generalInfo.js
+++ routes/generalInfo.js
@@ -25,40 +25,36 @@ router.get("/:token", (req, res) => {
 
 //permet de créer/éditer les infos generales de l'utilisateur
 //On cherche d'abord le user avec son token pour récuperer son id
-//puis on cherche dans la collection "generals" un un document General
-//qui contient l'id du user cherché précédemment , une fois trouvée
-//on modifie ce document avec General.findOne
+//puis on modifie via updateOne le document General associé au user id
 router.post("/update/:token", (req, res) => {
   User.findOne({ token: req.params.token }).then((data) => {
     if (data !== null) {
       console.log(data._id);
-      General.findOne({ user: data._id })
-        .populate("user")
-        .then((data) => {
-          console.log(data);
-          General.updateOne(
-            { _id: data._id },
-            {
-              general: {
-                birthDate: req.body.birthDate,
-                adress: req.body.adress,
-                description: req.body.description,
-                headline: req.body.headline,
-                yearExperience: req.body.yearExperience,
-                currentJob: req.body.currentJob,
-                // profilePicture: req.body.profilePicture,
-                // backgroundPicture: req.body.backgroundPicture,
-                urlLinkedIn: req.body.urlLinkedIn,
-              },
-            }
-          ).then((data) =>
-            res.json({
-              result: true,
-              res: "your profile was updated",
-              update: data,
-            })
-          );
-        });
+      //   General.findOne({ user: data._id })
+      //     .populate("user")
+      //     .then((data) => {
+      //       console.log(data);
+      General.updateOne(
+        { user: data._id },
+        {
+          general: {
+            birthDate: req.body.birthDate,
+            adress: req.body.adress,
+            description: req.body.description,
+            headline: req.body.headline,
+            yearExperience: req.body.yearExperience,
+            currentJob: req.body.currentJob,
+            // profilePicture: req.body.profilePicture,
+            // backgroundPicture: req.body.backgroundPicture,
+            urlLinkedIn: req.body.urlLinkedIn,
+          },
+        }
+      ).then((data) =>
+        res.json({
+          result: true,
+          res: "your profile was updated",
+        })
+      );
     }
   });
 });
